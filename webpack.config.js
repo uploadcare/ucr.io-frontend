37845e80637bba410d870a085f042a5a9c49f9e3
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-09-13 03:04:33 +0300

    Add js minification

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
 				toType: 'file',
 			},
 		]),
+		new webpack.DefinePlugin({'process.env': {NODE_ENV: JSON.stringify('production')}}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: false,
+			mangle: false,
+		}),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.NoErrorsPlugin(),
 	],
 	module: {
 		loaders: [
